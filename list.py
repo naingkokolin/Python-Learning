7d94e516dcb980dc192150812143fa97e798e7c2
naingkokolin <naingkokolin007@gmail.com>
2022-09-22 11:25:12 +0630

    list

--- list.py
+++ list.py
@@ -1,3 +1,8 @@
+from argparse import MetavarTypeHelpFormatter
+from cgi import print_arguments
+from mailcap import findmatch
+
+
 mylist = ['apple', 'orange', 'pineapple']
 print(mylist)
 
@@ -6,5 +11,20 @@ if "apple" in mylist:
 else:
     print(False)
 
-mylist.append("lemond")
+mylist.append("lemond")   # add an item at the end of list
+print(mylist)
+
+a=mylist.index("orange")  # return index no if there is item in list
+print(a)
+
+findmypineapple=mylist.index("pineapple", 0, 3)
+print(findmypineapple)
+
+mylist.insert(1,"strawberry")
+print(mylist)
+
+mylist.sort()
+print(mylist)
+
+mylist.clear()
 print(mylist)
